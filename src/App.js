f350e4bae53fc19248ae0e037fc1428d33bb1129
ty9249 <https://github.com/ty9249>
2025-08-23 16:35:38 +0900

    API 임시 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import VisitSchedulePage from "./pages/VisitSchedulePage";
+import InformationPage from "./pages/InformationPage";
+import RecommendPage from "./pages/RecommendPage";
+import InsertPlacePage from "./pages/InsertPlacePage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<InsertPlacePage  />} />
+        <Route path="/Recommendpage" element={<RecommendPage  />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
